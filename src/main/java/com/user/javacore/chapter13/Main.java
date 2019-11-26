b84fd690137e131deed8dc0224f8d691cb9c834f
Savinov Evgeny <Zsavinoww@gmail.com>
2019-11-26 22:51:53 +0300

    chapter13

---
+++ src/main/java/com/user/javacore/chapter13/Main.java
@@ -0,0 +1,17 @@
+package main.java.com.user.javacore.chapter13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        char c;
+         do {
+             c = (char)bufferedReader.read();
+             System.out.println(c);
+         } while (c != 'q');
+    }
+}
